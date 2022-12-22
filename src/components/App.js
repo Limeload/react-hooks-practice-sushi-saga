cfecaffcbdcbed501ac55aacf0a5af44bd9ac409
Shraddha Rao <shraddharaom@gmail.com>
2022-12-21 20:07:03 -0600

    commit tree 9314dc812e90ab35f78fe13d597d932b3622e681

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiArray, setSushiArray] = useState([])
+  const [wallet, setWallet] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then((sushiArray) => {
+        const updatedSushis = sushiArray.map((sushi) => {
+          return { ...sushi, eaten: false };
+        });
+        setSushiArray(updatedSushis);
+      });
+  }, []);
+
+  function handleEatSushi(eatenSushi) {
+    if (wallet >= eatenSushi.price) {
+      const updatedSushis = sushiArray.map((sushi) => {
+        if (sushi.id === eatenSushi.id) return { ...sushi, eaten: true };
+        return sushi;
+      });
+
+      setSushiArray(updatedSushis);
+      setWallet((wallet) => wallet - eatenSushi.price);
+    } else {
+      alert("Need more 💸");
+    }
+  }
+
+  function handleAddMoney(moreMoney) {
+    setWallet((wallet) => wallet + moreMoney);
+  }
+
+  const eatenSushis = sushiArray.filter((sushi) => sushi.eaten);
+
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushiArray = {sushiArray} onEatSushi = {handleEatSushi} />
+      <Table wallet={wallet} onAddMoney={handleAddMoney} plates={eatenSushis} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({sushiArray, onEatSushi }) {
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+  const sushiComponents = sushiArray
+    .slice(sushiIndex, sushiIndex + 4)
+    .map((sushi) => (
+      <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi} />
+    ));
+
+    function handleClickMore() {
+      setSushiIndex((sushiIndex) => (sushiIndex + 4) % sushiArray.length);
+    }
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onClickMore={handleClickMore}/>
     </div>
   );
 }
